277d4817ea6148db5ecca8d4b7c0d6b5e91c17cc
Rad Adricula <radricul@uci.edu>
2021-02-16 18:05:35 -0800

    created user class

--- app/src/main/java/com/example/solarise/MainActivity.java
+++ app/src/main/java/com/example/solarise/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.solarise;
 
 import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
@@ -17,72 +20,54 @@ import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
 public class MainActivity extends AppCompatActivity {
 
     private String TAG = "MainActivity";
     private FusedLocationProviderClient fusedLocationClient;
 
+    private EditText personName, personAge, personSleep, personHeight, personWeight;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        personName = (EditText) findViewById(R.id.person_name);
+        personAge = (EditText) findViewById(R.id.person_age);
+        personSleep = (EditText) findViewById(R.id.person_sleep);
+        personHeight = (EditText) findViewById(R.id.height);
+        personWeight = (EditText) findViewById(R.id.weight);
+
         Button btnSubmit = findViewById(R.id.submit);
 
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String userName = personName.getText().toString();
+                int userAge = Integer.parseInt(personAge.getText().toString());
+                boolean userSleep = personSleep.getText().toString().equals("Early Bird");
+                int userHeight = Integer.parseInt(personHeight.getText().toString());
+                int userWeight = Integer.parseInt(personWeight.getText().toString());
+                User user1 = new User(userName, userAge, userSleep, userHeight, userWeight);
+
+//                try {
+//                    FileOutputStream fos = openFileOutput("test.dat", Context.MODE_PRIVATE);
+//
+//                    // Wrapping our file stream
+//                    ObjectOutputStream oos = new ObjectOutputStream(fos);
+//                    oos.write(user1);
+//                    oos.close();
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
+
                 setContentView(R.layout.activity_dashboard);
             }
         });
 
-//        Button btnRequest = findViewById(R.id.btnRequest);
-//        Button btnLocate = findViewById(R.id.btnLocate);
-//
-//        // initialize the location client
-//        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-//
-//        btnRequest.setOnClickListener(new View.OnClickListener() {
-//            public void onClick(View v) {
-//                getLastLocation();
-//            }
-//        });
-//
-//        getLastLocation();
-
     }
-
-    private String getLastLocation() {
-        String msg = null;
-        Log.i(TAG, "getting last location");
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            requestPermission.launch(Manifest.permission.ACCESS_COARSE_LOCATION);
-        }
-        else {
-            fusedLocationClient.getLastLocation()
-                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                        @Override
-                        public void onSuccess(Location location) {
-                            // check if location was received
-                            if (location != null) {
-                                Log.i(TAG, "Location: " + location.toString());
-                                Log.i("MainActivity", location.toString());
-                                Log.i("MainActivity", Double.toString(location.getLatitude()));
-                            } else {
-                                Log.i(TAG, "error receiving location");
-                            }
-                        }
-                    });
-        }
-        return msg;
-
-    }
-
-    private ActivityResultLauncher<String> requestPermission = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
-           if (isGranted) {
-                Log.i("DEBUG", "permission granted");
-           }
-           else {
-               Log.i("DEBUG", "permission denied");
-           }
-        });
 }
