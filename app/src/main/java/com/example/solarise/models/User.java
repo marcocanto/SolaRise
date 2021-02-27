0a2bcf02a6cf7558c666efbc6abbc1906e2a541e
Rad Adricula <radricul@uci.edu>
2021-02-26 16:23:44 -0800

    took out height and weight

--- app/src/main/java/com/example/solarise/models/User.java
+++ app/src/main/java/com/example/solarise/models/User.java
@@ -8,20 +8,16 @@ public class User{
     private String name;
     private int age;
     private boolean earlyBird;
-    private int height;
-    private int weight;
     private int averageCaffeine;
     private List<Day> days = new ArrayList<Day>();
 
     public User() {}
 
-    public User(String name, int age, boolean earlyBird, int height, int weight, int averageCaffeine) {
+    public User(String name, int age, boolean earlyBird, int averageCaffeine) {
 
         this.name = name;
         this.age = age;
         this.earlyBird = earlyBird;
-        this.height = height;
-        this.weight = weight;
         this.averageCaffeine = averageCaffeine;
 
     }
@@ -31,10 +27,6 @@ public class User{
     public void setAge(int age) {this.age = age; }
 
     public void setSleepStatus(boolean sleepStatus) {this.earlyBird = sleepStatus; }
-
-    public void setHeight(int height) {this.height = height; }
-
-    public void setWeight(int weight) {this.weight = weight; }
 
     public void setAverageCaffeine(int averageCaffeine) {this.averageCaffeine = averageCaffeine;}
 
@@ -48,14 +40,6 @@ public class User{
 
     public boolean getSleepPreference() {
         return this.earlyBird;
-    }
-
-    public int getHeight() {
-        return this.height;
-    }
-
-    public int getWeight() {
-        return this.weight;
     }
 
     public int getAverageCaffeine() { return this.averageCaffeine; }
